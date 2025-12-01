c36b4cb011702fc5e5530aab6bdf4f22987f1324
TORRES240325 <torresxiter@gmail.com>
2025-12-01 13:47:21 -0500

    Update loader_main.py

--- loader_main.py
+++ loader_main.py
@@ -5,10 +5,7 @@ from dotenv import load_dotenv
 import subprocess
 
 # --- Configuración ---
-# RUTA EXPLÍCITA AL ARCHIVO .env (Asumiendo que está en la misma carpeta)
-# Esto garantiza que lo encuentre donde sea que lo ejecutes.
 load_dotenv(os.path.join(os.getcwd(), '.env'))
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 # --- Verificación de Token ---
@@ -16,12 +13,9 @@ TOKEN = os.getenv('BOT_MAIN_TOKEN')
 if not TOKEN:
     print("------------------------------------------------------------------")
     print("🚨 ERROR FATAL: BOT_MAIN_TOKEN no encontrado.")
-    print("Asegúrate de que BOT_MAIN_TOKEN esté definido en el archivo .env.")
-    print("------------------------------------------------------------------")
     sys.exit(1)
 
 print(f"Cargando BOT PRINCIPAL (Token: {TOKEN[:5]}...{TOKEN[-5:]})")
-print("El Bot se está iniciando. La consola se quedará activa.")
 
 try:
     subprocess.run([sys.executable, 'bot_main.py'])
